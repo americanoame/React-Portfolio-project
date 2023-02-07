6d3d75b061de882fb685385507c812a33a9cada7
Americano.Dev <andrenations@gmail.com>
2023-02-07 03:15:58 -0500

    refactored contact page

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -2,10 +2,8 @@
 const Contact = () => {
   return (
     <section id="contact" class="mb-4">
-
-      
       <h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
-      
+
       <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
         a matter of hours to help you.</p>
 
@@ -19,36 +17,36 @@ const Contact = () => {
             <div class="row">
 
               
-              <div class="col-md-6">
-                <div class="md-form mb-0">
+              <div class="col-md-9">
+                <div class="md-form mb-2">
                   <input type="text" id="name" name="name" class="form-control" />
-                    <label for="name" class="">Your name</label>
+                    <label for="name" class="label-input">Your name</label>
                 </div>
               </div>
             
-              <div class="col-md-6">
+              <div class="col-md-9">
                 <div class="md-form mb-0">
                   <input type="text" id="email" name="email" class="form-control" />
-                    <label for="email" class="">Your email</label>
+                    <label for="email" class="label-input">Your email</label>
                 </div>
               </div>
               
             </div>
                       
             <div class="row">
-              <div class="col-md-12">
+              <div class="col-md-9">
                 <div class="md-form mb-0">
                   <input type="text" id="subject" name="subject" class="form-control" />
-                    <label for="subject" class="">Subject</label>
+                    <label for="subject" class="label-input">Subject</label>
                 </div>
               </div>
             </div>
             <div class="row">
-              <div class="col-md-12">
+              <div class="col-md-9">
 
                 <div class="md-form">
                   <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
-                  <label for="message">Your message</label>
+                  <label for="message" class="label-input">Your message</label>
                 </div>
 
               </div>
@@ -57,22 +55,22 @@ const Contact = () => {
           </form>
 
           <div class="text-center text-md-left">
-            <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
+            <a  class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
           </div>
           <div class="status"></div>
         </div>
         <div class="col-md-3 text-center">
           <ul class="list-unstyled mb-0">
             <li><i class="fas fa-map-marker-alt fa-2x"></i>
-              <p>San Francisco, CA 94126, USA</p>
+              <p class="contac-address">New Jersey, NJ 07032, USA</p>
             </li>
 
             <li><i class="fas fa-phone mt-4 fa-2x"></i>
-              <p>+ 01 234 567 89</p>
+              <p class="contac-address">+1 (848) 468-8266</p>
             </li>
 
             <li><i class="fas fa-envelope mt-4 fa-2x"></i>
-              <p>contact@mdbootstrap.com</p>
+              <p class="contac-address">Andrenations@gmail.com</p>
             </li>
           </ul>
         </div>
